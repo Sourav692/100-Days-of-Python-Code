1ab86e688208c52e351ae0e4c4d14c811c1ab544
Sourav Banerjee <sourav692@gmail.com>
2021-10-11 03:28:57 +0530

    Test Giutkraken

--- Day 1/10. get_and_print_string.py
+++ Day 1/10. get_and_print_string.py
@@ -1,4 +1,4 @@
-# Write a Python class which has two methods get_String and print_String.
+# Write a Python class which has two methods get_String and print_String....
 # get_String accept a string from the user and print_String print the string in upper case.
 
 class IOString():
